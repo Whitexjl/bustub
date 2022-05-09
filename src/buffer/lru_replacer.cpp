41ebc03d3a732c199d29a7ad3bb43144694f7a2e
xuanjinlong <15563983051@163.com>
2022-05-09 19:50:48 +0800

    lru0

--- src/buffer/lru_replacer.cpp
+++ src/buffer/lru_replacer.cpp
@@ -14,16 +14,49 @@
 
 namespace bustub {
 
-LRUReplacer::LRUReplacer(size_t num_pages) {}
+LRUReplacer::LRUReplacer(size_t num_pages) {
+    lruListMaxSize = num_pages;
+}
 
 LRUReplacer::~LRUReplacer() = default;
 
-bool LRUReplacer::Victim(frame_id_t *frame_id) { return false; }
+// 淘汰
+bool LRUReplacer::Victim(frame_id_t *frame_id) { 
+    std::scoped_lock mtxLock(mtx);
+    if(lruList.size() == 0)
+        return false; 
 
-void LRUReplacer::Pin(frame_id_t frame_id) {}
+    *frame_id = lruList.back();
+    lruHashMap.erase(*frame_id);
+    lruList.pop_back();
 
-void LRUReplacer::Unpin(frame_id_t frame_id) {}
+    return true;
+ }
 
-size_t LRUReplacer::Size() { return 0; }
+// 删除
+void LRUReplacer::Pin(frame_id_t frame_id) {
+    std::scoped_lock mtxLock(mtx);
+    if(lruHashMap.count(frame_id) == 0)
+        return ; 
+
+    auto frame_id_itr = lruHashMap[frame_id];
+    lruList.erase(frame_id_itr);
+    lruHashMap.erase(frame_id);
+}
+
+// 增加
+void LRUReplacer::Unpin(frame_id_t frame_id) {
+    std::scoped_lock mtxLock(mtx);
+    if (lruHashMap.count(frame_id) != 0) 
+        return;
+
+    if(lruList.size() == lruListMaxSize)
+        return ; 
+
+    lruList.push_front(frame_id);
+    lruHashMap.emplace(frame_id, lruList.begin());
+}
+
+size_t LRUReplacer::Size() { return lruList.size(); }
 
 }  // namespace bustub
